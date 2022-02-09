3b92bfcebef314225b933f249aa1382091ff62b6
bomurphy <bomurphy@iu.edu>
2022-02-09 02:10:04 -0500

    editing login.js functionality

--- Login/login.js
+++ Login/login.js
@@ -1,16 +1,16 @@
 //Using jQuery to add funtion to google sign in, sign out buttons
 function onSignIn(googleUser) {
 
-    if  (profile != "" || profile != undefined || profile != null){  
-        window.location.href = 'https://cgi.luddy.indiana.edu/~team51/Restaurant/restaurant.html'
-    }
-
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $("#image").attr('src', profile.getImageUrl());
     $(".data").css("display", "block");
     $(".g-signin2").css("display", "none");
+
+    if  (profile != "" || profile != undefined || profile != null){  
+        window.location.href = 'https://cgi.luddy.indiana.edu/~team51/Restaurant/restaurant.html'
+    }
 }
 
 function signOut() {
