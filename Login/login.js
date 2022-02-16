3b70be7c56dfcb3402e3f5785dab8d61562cdd4c
bomurphy <bomurphy@iu.edu>
2022-02-16 16:18:03 -0500

    adding funtionality to the restaurants tab. added the discovery tab and funtionality for the filter page.

--- Login/login.js
+++ Login/login.js
@@ -1,23 +1,59 @@
 //Using jQuery to add funtion to google sign in, sign out buttons
+
+
+let signOut = document.querySelector('.signOutBtn')                
+
 function onSignIn(googleUser) {
 
-    var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
+    // var profile = googleUser.getBasicProfile();
+    // $("#name").text(profile.getName());
+    // $("#email").text(profile.getEmail());
+    // $("#image").attr('src', profile.getImageUrl());
+    // $(".data").css("display", "block");
+    // $(".g-signin2").css("display", "none");
 
-    if  (profile != "" || profile != undefined || profile != null){  
-        window.location.href = 'https://cgi.luddy.indiana.edu/~team51/Restaurant/restaurant.html'
+    const profile = googleUser.getBasicProfile();
+    userImg = profile.getImageUrl();
+    userName = profile.getName();
+    userEmail = profile.getEmail();
+
+// set profile to object
+// JSON Parse session to get data and pass the data
+
+    if  (profile != "" || profile != undefined || profile != null) {  
+        // window.location.href = 'https://cgi.luddy.indiana.edu/~bomurphy/capstone-individual/Restaurant/restaurant.html'
+        let loginBtn = document.querySelector('#login-btn')        
+        let registerBtn = document.querySelector('#register-btn')        
+        let user = document.querySelector('.user');
+
+        sessionStorage.setItem('username', `${userName}`);
+        sessionStorage.setItem('useremail', `${userEmail}`);
+        sessionStorage.setItem('userimage', `${userImg}`);
+
+        let userIcon = `<img src='${sessionStorage.getItem('userimage')}' alt='user_img' />        `;        
+        
+        loginBtn.classList.add('hide');        
+        registerBtn.classList.add('hide');        
+        user.classList.remove('hide')        
+        user.insertAdjacentHTML('afterbegin', userIcon)        
+        signOut.classList.remove('hide');        
+        // signOut.addEventListener('click', googleSignOut())
+
     }
 }
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
+function googleSignOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         alert("You have been signed out successfully");
         $(".data").css("display", "none");
-        $(".g-signin2").css("display", "block");
+
+
+        // Timer to redirect back to login screen after sign out        
+$(".g-signin2").css("display", "block");
+
+
+        
+
     });
 }
---
+++ Restaurant/restaurant.js
@@ -0,0 +1,75 @@
+const loginBtn = document.querySelector('#login-btn')
+const registerBtn = document.querySelector('#register-btn')
+let user = document.querySelector('.user')
+let signOutBtn = document.querySelector('.signOutBtn')
+
+if (sessionStorage.getItem('useremail') != '' || sessionStorage.getItem('useremail') != undefined || sessionStorage.getItem('useremail') != null) {
+    registerBtn.classList.add('hide')
+    loginBtn.classList.add('hide')
+    user.classList.remove('hide')
+    signOutBtn.classList.remove('hide')
+
+    let userIcon = `<img src="${sessionStorage.getItem('userimage')}" />`;
+
+    user.insertAdjacentHTML('afterbegin', userIcon)
+
+
+}
+
+
+
+// //Using jQuery to add funtion to google sign in, sign out buttons
+
+
+// let signOut = document.querySelector('.signOutBtn')                
+
+// function onSignIn(googleUser) {
+
+//     // var profile = googleUser.getBasicProfile();
+//     // $("#name").text(profile.getName());
+//     // $("#email").text(profile.getEmail());
+//     // $("#image").attr('src', profile.getImageUrl());
+//     // $(".data").css("display", "block");
+//     // $(".g-signin2").css("display", "none");
+
+//     const profile = googleUser.getBasicProfile();
+//     userImg = profile.getImageUrl();
+//     userName = profile.getName();
+//     userEmail = profile.getEmail();
+//     if  (profile != "" || profile != undefined || profile != null) {  
+//         window.location.href = 'https://cgi.luddy.indiana.edu/~bomurphy/capstone-individual/Restaurant/restaurant.html'
+//         let loginBtn = document.querySelector('#login-btn')        
+//         let registerBtn = document.querySelector('#register-btn')        
+//         let user = document.querySelector('.user');        
+//         let userIcon = `<img src='${userImg}' alt='user_img' />        `;        
+        
+//         loginBtn.classList.add('hide');        
+//         registerBtn.classList.add('hide');        
+//         user.classList.remove('hide')        
+//         user.insertAdjacentHTML('afterbegin', userIcon)        
+//         signOut.classList.remove('hide');        
+//         // signOut.addEventListener('click', googleSignOut())
+
+//         sessionStorage.setItem('username', `${userName}`);
+//         sessionStorage.setItem('useremail', `${userEmail}`);
+//         sessionStorage.setItem('userimage', `${userImg}`);
+
+//     }
+// }
+
+// function googleSignOut() {
+//     const auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//         alert("You have been signed out successfully");
+//         $(".data").css("display", "none");
+
+
+//         // Timer to redirect back to login screen after sign out        
+// $(".g-signin2").css("display", "block");
+
+
+        
+
+//     });
+
+// }
