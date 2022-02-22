d795ae23cd8892ab4ae1f7ac4472eefb337f5d00
Finn McSweeney <fmcsween@iu.edu>
2022-02-22 12:14:37 -0500

    Changed made to file names

--- Register/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("db.luddy.indiana.edu", "i494f21_fmcsween", "my+sql=i494f21_fmcsween", "i494f21_fmcsween");
-
-if (!con) {
-	die("Failed to connect to MySQL: " . mysqli_connect_error());
-} else
-	{echo "Established Database Connection";}
-?>
