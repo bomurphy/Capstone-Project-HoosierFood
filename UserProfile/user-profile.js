7c7ce2372a659fa2a1196f185d3fb2b982031984
bomurphy <bomurphy@iu.edu>
2022-02-22 17:16:05 -0500

    editing sign out button functionality.

--- UserProfile/user-profile.js
+++ UserProfile/user-profile.js
@@ -36,12 +36,8 @@ else {
 
 
     function googleSignOut() {
-        sessionStorage.removeItem('useremail')
-        sessionStorage.removeItem('username')
-        sessionStorage.removeItem('userimage')
-        user.style.display = 'none'
-        loginBtn.classList.remove('hide')
-
-    
+        document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=https://cgi.luddy.indiana.edu/~team51/Login/login.html";
     }
+
 signOutBtn.addEventListener('click', googleSignOut)
+
