44588050dafa078b8931b3d3c5f5e5f4ad499904
Finn McSweeney <fmcsween@iu.edu>
2022-02-22 14:23:22 -0500

    Updating php file

---
+++ Register/index.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'server.php';
+if(isset($_POST['submit']))
+{    
+     $FirstName = $_POST['FirstName'];
+     $LastName = $_POST['LastName'];
+     $Street = $_POST['Street'];
+	 $City = $_POST['City'];
+	 $State = $_POST['State'];
+	 $Zip = $_POST['Zip'];
+	 $Email = $_POST['Email'];
+	 $username = $_POST['username'];
+	 $password = $_POST['password'];
+     $sql = "INSERT INTO users (FirstName, LastName, Street, City, State, Zip, Email, username, password)
+     VALUES ('$FirstName','$LastName','$Street', '$City', '$State', '$Zip', '$Email', '$username', '$password')";
+     if (mysqli_query($con, $sql)) {
+        echo "New record has been added successfully !";
+     } else {
+        echo "Error: " . $sql . ":-" . mysqli_error($conn);
+     }
+     mysqli_close($conn);
+}
+?>
