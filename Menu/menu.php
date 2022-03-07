3765b3d05c0386a5088d43e632e98d0cd841c8ce
bomurphy <bomurphy@iu.edu>
2022-03-07 15:03:52 -0500

    editing database connection for profile page

--- Menu/menu.php
+++ Menu/menu.php
@@ -6,22 +6,22 @@
     } else {
         {echo "connection made" . "<br>";}
 
-        $sql = "SELECT RestaurantName, menuURL FROM menus";
+        $sql = "SELECT * FROM menus";
 
         $result = $con->query($sql);
 
-        if (mysqli_num_rows($result) > 0) {
-            echo "<table>";
-            echo "<tr><th>Restaurant</th><th>URL</th></tr>";
+        // if (mysqli_num_rows($result) > 0) {
+        //     echo "<table>";
+        //     echo "<tr><th>Restaurant</th><th>URL</th></tr>";
 
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr><td>".$row['RestaurantName']."</td><td>".$row['menuURL']."</td></tr>";
-            }
+        //     while ($row = $result->fetch_assoc()) {
+        //         echo "<tr><td>".$row['RestaurantName']."</td><td>".$row['menuURL']."</td></tr>";
+        //     }
 
-            echo "</table>";
-        } else {
-            echo "0 results <br>";
-        }
+        //     echo "</table>";
+        // } else {
+        //     echo "0 results <br>";
+        // }
 
     }
 ?>
