d08294bf58e2e55b3e93a529285fc5be0e2ad11a
bomurphy <bomurphy@iu.edu>
2022-03-07 15:50:25 -0500

    Editing styling in menus feature

--- Menu/menu.php
+++ Menu/menu.php
@@ -56,6 +56,10 @@ function filterTable($query)
 <html>
     <head>
         <title>Menu</title>
+        <meta name="google-signin-client_id" 
+        content="1027531663911-al3p7o7qrb5eqdpq6j3ea1mn63tvg5jc.apps.googleusercontent.com">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <style>
             body {
                 background-color: beige;
@@ -63,7 +67,45 @@ function filterTable($query)
         </style>
     </head>
     <body>
-        
+    nav class="navbar navbar-expand-lg p-2 border-bottom nav-background">
+        <div class="container-fluid">
+          <img src="../photos/iu-logo.svg" class="iu-logo me-5" alt="">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active red-text" aria-current="page" href="../Restaurant/restaurant.html">Restaurants</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link red-text" href="../Orders/orders.php">Orders</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link red-text" href="#" tabindex="-1" aria-disabled="true">Reviews</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link red-text" href="../Filter/filter.php" tabindex="-1" aria-disabled="true">Discovery</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link red-text" href="#" tabindex="-1" aria-disabled="true">Support</a>
+              </li>
+            </ul>
+            <!-- <form class="d-flex">
+              <button class="btn btn-outline-danger me-1">Login</button>
+              <button class="btn btn-outline-danger" type="submit">Register</button>
+            </form> -->
+            <form action="" class="d-flex">
+              <a href='../UserProfile/user-profile.html' class="user"></a>
+                <!-- <button id="login-btn" class="login me-2">Login</button>
+                
+                <a href="https://cgi.luddy.indiana.edu/~team51/Register/register.php" id="register-btn" class="login">Register</button></a>
+                <button class="signOutBtn login hide">Sign Out</button> -->
+                <a href="#" onclick="signOut();"><button class="login signOutBtn">Sign Out</button></a>
+            </form>
+          </div>
+        </div>
+      </nav>
 		<h2>Menu</h2>
 		
 		<table>
@@ -84,7 +126,7 @@ function filterTable($query)
                 <tr>
                     <td><?php echo $row['RestaurantName'];?></td>
                     <td><?php
-                        $HTML = "<a href=".$row['menuURL'].">"."Link"."</a>";
+                        $HTML = "<a href=".$row['menuURL'].">".$row['menuURL']."</a>";
                         echo $HTML;
                     ?></td>
                 </tr>
@@ -92,5 +134,11 @@ function filterTable($query)
             </table>
         </form>
         
+        <script src="https://apis.google.com/js/platform.js" async defer></script> 
+    <!--adding bootstrap and jQuery extentions-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+        crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </body>
 </html>
