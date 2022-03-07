ce47539c59e6a4b83dcb5a7eae51c7fb64c5aa9f
bomurphy <bomurphy@iu.edu>
2022-03-07 15:12:14 -0500

    Creating menus feature and editing database connection

--- Menu/menu.php
+++ Menu/menu.php
@@ -1,5 +1,5 @@
 <?php 
-    $connect = mysqli_connect("db.soic.indiana.edu", "i494f21_team51", "my+sql=i494f21_team51", "i494f21_team51");
+    $connect = mysqli_connect("db.sice.indiana.edu", "i494f21_team51", "my+sql=i494f21_team51", "i494f21_team51");
 
     if (!$connect) {
         die ("failed to connect" . mysqli_connect_error());
@@ -10,43 +10,18 @@
 
         $result = $con->query($sql);
 
-        // if (mysqli_num_rows($result) > 0) {
-        //     echo "<table>";
-        //     echo "<tr><th>Restaurant</th><th>URL</th></tr>";
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table>";
+            echo "<tr><th>Restaurant</th><th>URL</th></tr>";
 
-        //     while ($row = $result->fetch_assoc()) {
-        //         echo "<tr><td>".$row['RestaurantName']."</td><td>".$row['menuURL']."</td></tr>";
-        //     }
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>".$row['RestaurantName']."</td><td>".$row['menuURL']."</td></tr>";
+            }
 
-        //     echo "</table>";
-        // } else {
-        //     echo "0 results <br>";
-        // }
+            echo "</table>";
+        } else {
+            echo "0 results <br>";
+        }
 
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
-</head>
-<body>
-    <div class="">
-        <table>
-            <tr>
-                <td>Restaurant</td>
-                <td>URL</td>
-            </tr>
-            <?php while ($row = $result->fetch_assoc()):?>
-            <tr>
-                <td><?php echo $row['RestaurantName'];?></td>
-                <td><?php echo $row['menuURL'];?></td>
-            </tr>
-            <?php endwhile;?>
-        </table>
-    </div>
-</body>
-</html>
