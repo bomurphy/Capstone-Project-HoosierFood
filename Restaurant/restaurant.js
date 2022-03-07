39a1fff67306fde2ff14602fc0f9576d72197c4d
bomurphy <bomurphy@iu.edu>
2022-03-07 16:34:28 -0500

    Editing restaurant page to have profile icon

--- Restaurant/restaurant.js
+++ Restaurant/restaurant.js
@@ -1,43 +1,28 @@
 let user = document.querySelector('.user')
-let signOutBtn = document.querySelector('.signOutBtn')
-
-let userIcon = `<img class="user-icon-img" src="${sessionStorage.getItem('userimage')}" />`;
+let userNameCont = document.querySelector('.name');
+let userEmailCont = document.querySelector('.email');
 
 if (sessionStorage.getItem('useremail') != '' || sessionStorage.getItem('useremail') != undefined || sessionStorage.getItem('useremail') != null) {
-    
+    user.classList.remove('hide')
+    let userIcon = `<img class="user-icon-img" src="${sessionStorage.getItem('userimage')}" />`;
+
+    let name = sessionStorage.getItem('username');
+    let email = sessionStorage.getItem('useremail');
+
+    userNameCont.insertAdjacentHTML('afterbegin', name);
+    userEmailCont.insertAdjacentHTML('afterbegin', sessionStorage.getItem('useremail'))
 
 
     user.insertAdjacentHTML('afterbegin', userIcon)
 
 
-} else {
-    console.log('broke')
+}
+else {
+    console.log('yay')
+    
 }
 
 
 
-    
-    // loginBtn.classList.remove('hide')
-
-
-// signOutBtn.addEventListener('click', function () {
-//     var auth2 = gapi.auth2.getAuthInstance();
-//     auth2.signOut().then(function () {
-//         alert("You have been signed out successfully");
-//         $(".data").css("display", "none");
-//         $(".g-signin2").css("display", "block");
-//         loginBtn.classList.remove('hide')
-//         window.location="https://cgi.luddy.indiana.edu/~team51/Login/login.html"
-
-//     });
-    //"
-// })
-
-
-
-
-
-
-
-
+signOutBtn.addEventListener('click', googleSignOut)
 
